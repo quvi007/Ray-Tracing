976dc313a561c2f10ac70baac75924eb50cf737c
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2023-08-31 03:11:59 +0600

    class defs

--- 1805007_classes.h
+++ 1805007_classes.h
@@ -1,25 +1,81 @@
+#include <bits/stdc++.h>
+
 class Vector3D {
+private:
+    friend istream &operator>>(istream &is, Vector3D &vector3D);
+    friend ostream &operator<<(ostream &os, const Vector3D &vector3D);
 public:
     double x, y, z;
 };
 
-class Object {
+class Color {
+private:
+    friend istream &operator>>(istream &is, Color &color);
+    friend ostream &operator<<(ostream &is, const Color &color);
 public:
-    Vector3D reference_point; // should have x, y, z
-    double height, width, length;
-    double color[3];
-    double coEfficients[4]; // ambient, diffuse, specular, reflection coefficients
-    int shine; // exponent term of specular component
-    Object() {}
-    virtual void draw(){}
-    void setColor(){}
-    void setCoEfficients(){}
-    void setShine(){}
+    double r, g, b;
 };
 
-class Sphere : Object {
+class CoEfficients {
+private:
+    friend istream &operator>>(istream &is, CoEfficients &coEfficients);
+    friend ostream &operator<<(ostream &os, const CoEfficients &coEfficients);
+public:
+    double ambient;
+    double diffuse;
+    double specular;
+    double reflection;
+};
+
+class Object {
+protected:
+    Vector3D referencePoint; // should have x, y, z
+    double height, width, length;
+    Color color;
+    CoEfficients coEfficients; // ambient, diffuse, specular, reflection coEfficients
+    int shine; // exponent term of specular component
+public:
+    Object() {}
+
+    virtual void draw() {}
+    
+    void setReferencePoint(const Vector3D &referencePoint) {
+        this->referencePoint = referencePoint;
+    }
+
+    void setColor(const Color &color) {
+        this->color = color;
+    }
+
+    void setCoEfficients(const CoEfficients &coEfficients) {
+        this->coEfficients = coEfficients;
+    }
+    
+    void setShine(int shine) {
+        this->shine = shine;
+    }
+
+    Vector3D getReferencePoint() const {
+        return referencePoint;
+    }
+
+    Color getColor() const {
+        return color;
+    }
+
+    CoEfficients getCoEfficients() const {
+        return coEfficients;
+    }
+
+    int getShine() const {
+        return shine;
+    }
+};
+
+class Sphere : public Object {
+public:
     Sphere(const Vector3D &center, double radius) {
-        this->reference_point = center;
+        this->referencePoint = center;
         this->length = radius;
     }
     void draw() {
@@ -27,7 +83,8 @@ class Sphere : Object {
     }
 };
 
-class Triangle : Object {
+class Triangle : public Object {
+public:
     Triangle(const Vector3D &p1, const Vector3D &p2, const Vector3D &p3) {
         // what will be the triangle's constructor be like?
     }
@@ -36,14 +93,123 @@ class Triangle : Object {
     }
 };
 
-class Quad : Object {
-
+class General : public Object {
+public:
+    General() {
+        // write this constructor
+    }
+    void draw() {
+        // write codes for drawing quad object
+    }
 };
 
 class PointLight {
+private:
+    Vector3D lightPos;
+    Color color;
 
+    friend istream &operator>>(istream &is, PointLight &pointLight);
+    friend ostream &operator<<(ostream &os, const PointLight &pointLight);
+public:
+    PointLight() {}
+    PointLight(const Vector3D &lightPos, const Color &color) {
+        this->lightPos = lightPos;
+        this->color = color;
+    }
+
+    void setLightPos(const Vector3D &lightPos) {
+        this->lightPos = lightPos;
+    }
+
+    void setColor(const Color &color) {
+        this->color = color;
+    }
+
+    Vector3D getLightPos() const {
+        return lightPos;
+    }
+
+    Color getColor() const {
+        return color;
+    }
+    
 };
 
 class SpotLight {
+private:
+    PointLight pointLight;
+    Vector3D lightDirection;
+    double cutoffAngle;
+public:
+    SpotLight() {}
+    SpotLight(const PointLight &pointLight, const Vector3D &lightDirection, double cutoffAngle) {
+        this->pointLight = pointLight;
+        this->lightDirection = lightDirection;
+        this->cutoffAngle = cutoffAngle;
+    }
+
+    void setPointLight(const PointLight &pointLight) {
+        this->pointLight = pointLight;
+    }
+
+    void setLightDirection(const Vector3D &lightDirection) {
+        this->lightDirection = lightDirection;
+    }
+
+    void setCutoffAngle(double cutoffAngle) {
+        this->cutoffAngle = cutoffAngle;
+    }
+
+    PointLight getPointLight() const {
+        return pointLight;
+    }
+
+    Vector3D getLightDirection() const {
+        return lightDirection;
+    }
+
+    double getCutoffAngle() const {
+        return cutoffAngle;
+    }
 
 };
+
+istream &operator>>(istream &is, Vector3D &vector3D) {
+    is >> vector3D.x >> vector3D.y >> vector3D.z;
+    return is;
+}
+
+ostream &operator<<(ostream &os, const Vector3D &vector3D) {
+    os << vector3D.x << " " << vector3D.y << " " << vector3D.z;
+    return os;
+}
+
+istream &operator>>(istream &is, Color &color) {
+    is >> color.r >> color.g >> color.b;
+    return is;
+}
+
+ostream &operator<<(ostream &os, const Color &color) {
+    os << color.r << " " << color.g << " " << color.b;
+    return os;
+}
+
+istream &operator>>(istream &is, CoEfficients &coEfficients) {
+    is >> coEfficients.ambient >> coEfficients.diffuse >> coEfficients.specular >> coEfficients.reflection;
+    return is;
+}
+
+ostream &operator<<(ostream &os, const CoEfficients &coEfficients) {
+    os << coEfficients.ambient << " " << coEfficients.diffuse << " " << coEfficients.specular << " " << coEfficients.reflection;
+    return os;
+}
+
+istream &operator>>(istream &is, PointLight &pointLight) {
+    is >> pointLight.lightPos >> pointLight.color;
+    return is;
+}
+
+ostream &operator<<(ostream &os, const PointLight &pointLight) {
+    os << pointLight.lightPos << " " << pointLight.color;
+}
+
--- 1805007_main.cpp
+++ 1805007_main.cpp
@@ -9,24 +9,72 @@ int level_of_recursion;
 int dimension;
 
 int number_of_objects;
+int number_of_point_light_sources;
+int number_of_spot_light_sources;
+
+// declaration
+
+extern vector<Object *> objects;
+extern vector<PointLight *> pointLights;
+extern vector<SpotLight *> spotLights;
 
 void loadData() {
     ifstream ifs(scene_file);
     ifs >> level_of_recursion;
     ifs >> dimension;
+    
     ifs >> number_of_objects;
     for (int i = 0; i < number_of_objects; ++i) {
         string obj_type;
         ifs >> obj_type;
         if (obj_type == "sphere") {
-            
+            Vector3D center;
+            double radius;
+            Color color;
+            CoEfficients coEfficients;
+            int shine;
+
+            ifs >> center >> radius >> color >> coEfficients >> shine;
+
+            Object *sphere = new Sphere(center, radius);
+
+            sphere->setColor(color);
+            sphere->setCoEfficients(coEfficients);
+            sphere->setShine(shine);
+
+            objects.push_back(sphere);
         } else if (obj_type == "triangle") {
 
         } else if (obj_type == "general") {
 
         }
     }
-    ifs.close();    
+
+    ifs >> number_of_point_light_sources;
+    for (int i = 0; i < number_of_point_light_sources; ++i) {
+        Vector3D lightPos;
+        Color color;
+        
+        ifs >> lightPos;
+        ifs >> color;
+
+        PointLight *pointLight = new PointLight(lightPos, color);
+        pointLights.push_back(pointLight);
+    }
+
+    ifs >> number_of_spot_light_sources;
+    for (int i = 0; i < number_of_spot_light_sources; ++i) {
+        PointLight pointLight;
+        Vector3D lightDirection;
+        double cutoffAngle;
+
+        ifs >> pointLight >> lightDirection >> cutoffAngle;
+
+        SpotLight *spotLight = new SpotLight(pointLight, lightDirection, cutoffAngle);
+        spotLights.push_back(spotLight);
+    }
+
+    ifs.close(); 
 }
 
 int main(int argc, char *argv[]) {
